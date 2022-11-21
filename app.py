900d0f10cae42fa983161b7744933750f2d7ba8b
igorpereira28 <igorsilvape28@gmail.com>
2022-11-21 02:25:51 -0300

    Incluindo flask no código

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/homepage")
+
+
+def homepage(): 
+
+    return render_template("index.html")
+
+
+@app.route("/portfolio")
+
+
+def contato():
+    return render_template("portfolio.html")
+
+    
+
+@app.route("/curriculum")
+
+
+def quemsomos():
+    return render_template("curriculum.html")
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
